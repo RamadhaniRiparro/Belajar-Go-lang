e39c6a5e0294b0301fb028583b9d2f41e3ffc495
RamadhaniRiparro <ramadhani9112@gmail.com>
2021-01-21 15:27:09 +0700

    first

---
+++ integer.go
@@ -0,0 +1,21 @@
+package main
+
+
+
+func main() {
+	// print 6
+	println(6)
+
+	//print 75
+	println(75)
+
+    // Print the result of 12 divided by 3
+    println(12/3)
+    
+    // Print the result of 3 times 6
+    println(3*6)
+    
+    // Print the remainder of 8 divided by 3
+    println(8%3)
+    
+}
